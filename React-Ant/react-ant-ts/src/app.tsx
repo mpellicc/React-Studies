cac5517701fbd2e8ebfef0dead708c520b232826
mpellicc <mpelliccione.sub@gmail.com>
2022-01-21 14:17:50 +0100

    Rebuilt app with `create-react-app --template typescript`

---
+++ React-Ant/react-ant-ts/src/app.tsx
@@ -0,0 +1,12 @@
+import Product from "./components/product/Product";
+
+function App(props) {
+    
+    return (
+        <div>
+            <Product />
+        </div>
+    );
+}
+
+export default App;
