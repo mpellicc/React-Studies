cbac63f7c80bb845d00dcae7ea66f63413755469
mpellicc <mpelliccione.sub@gmail.com>
2022-02-02 12:13:58 +0100

    Formatting

--- React-Ant/react-ant-ts/src/features/test-page/components/ReduxProduct.tsx
+++ React-Ant/react-ant-ts/src/features/test-page/components/ReduxProduct.tsx
@@ -12,25 +12,27 @@ function ReduxSingleProduct() {
   /* const prod = useSelector((state: any) =>
     state.products.find((item: any) => item.id === n_id)
   ); */
-  const prod = useSelector((state: RootState) => selectProdById(state, n_id))
+  const prod = useSelector((state: RootState) => selectProdById(state, n_id));
 
   if (!prod) {
     return (
       <Card bordered={false} style={{ margin: "50px 0" }}>
-        <Empty description={
-          <p><Text type="danger">Error</Text> - Product Not Found</p>
-        } />
+        <Empty
+          description={
+            <p>
+              <Text type="danger">Error</Text> - Product Not Found
+            </p>
+          }
+        />
       </Card>
-    )
+    );
   }
   return (
     <Card bordered={false} style={{ margin: "50px 0" }}>
       <Row gutter={16}>
         <Col span={16}>
           <Card bordered={false} type="inner">
-            <Typography.Title>
-              {prod.title}
-            </Typography.Title>
+            <Typography.Title>{prod.title}</Typography.Title>
             <Typography.Title level={5} type="secondary">
               {prod.category}
             </Typography.Title>
